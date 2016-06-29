52f4053b27651515ecb9a4f2932561096b5e2e8a
cybermasterdza <cybermasterdza@gmail.com>
2016-06-29 13:31:55 -0300

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -4,4 +4,5 @@ console.log("comando executado: grávida, pêndulo...");
 $(document).ready(function(){
   console.log("jquery funciona com perfeição: Pêndulo, grávida!");
   console.log("jquery adicionado com sucesso, agora testando o refresh!");
+  console.log("Para forçar o refresh da página é preciso usar crt+f5 on Chrome!");
 });
